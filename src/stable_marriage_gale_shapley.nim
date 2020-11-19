08830851c21ba590bd59b000415347b38823b5ff
ZoomRmc <zoomrmc@gmail.com>
2020-11-20 02:09:39 +0300

    Replace std/enumerate import with 'pairs' iterator

--- src/stable_marriage_gale_shapley.nim
+++ src/stable_marriage_gale_shapley.nim
@@ -1,4 +1,4 @@
-import sequtils, std/enumerate, random, strutils
+import sequtils, random, strutils
 const Pairs = 10
 const MNames = ["abe", "bob", "col", "dan", "ed", "fred", "gav", "hal", "ian", "jon"]
 const FNames = ["abi", "bea", "cath", "dee", "eve", "fay", "gay", "hope", "ivy", "jan"]
@@ -30,22 +30,22 @@ const FPreferences = [
 # recipient's preferences hold the preference score for each contender's id
 func getRecPreferences[N: static int](prefs: array[N, array[N, string]],
     names: openArray[string]): array[N, array[N, int]] {.compileTime.} =
-  for r, prefArray in enumerate(prefs):
-    for c, contender in enumerate(prefArray):
+  for r, prefArray in pairs(prefs):
+    for c, contender in pairs(prefArray):
       result[r][c] = prefArray.find(MNames[c])
 
 # contender's preferences hold the recipient ids in descending order of preference
 func getContPreferences[N: static int](prefs: array[N, array[N, string]], names: openArray[
     string]): array[N, array[N, int]] {.compileTime.} =
-  for c, pref_seq in enumerate(prefs):
-    for r, pref in enumerate(pref_seq):
+  for c, pref_seq in pairs(prefs):
+    for r, pref in pairs(pref_seq):
       result[c][r] = names.find(pref)
 
 const RecipientPrefs = getRecPreferences(FPreferences, MNames)
 const ContenderPrefs = getContPreferences(MPreferences, FNames)
 
 proc printCoupleNames(contPairs: seq[int]) =
-  for c, r in enumerate(contPairs):
+  for c, r in pairs(contPairs):
     echo MNames[c] & " 💑" & FNames[contPairs[c]]
 
 func pair(): (seq[int], seq[int]) =
@@ -82,7 +82,7 @@ proc randomPair(max: int): (int, int) =
 proc perturbPairs(contPairs, recPairs: var seq[int]) =
   randomize()
   let (a,b) = randomPair(Pairs-1)
-  echo "Swapping " & MNames[a] & " & " & MNames[b] & " partners"
+  echo("Swapping ", MNames[a], " & ", MNames[b], " partners")
   swap(contPairs[a], contPairs[b])
   swap(recPairs[contPairs[a]], recPairs[contPairs[b]])
 
@@ -94,10 +94,8 @@ proc checkPairStability(contPairs, recPairs: seq[int]): bool =
       let curRecPair = recPairs[checkedRec] # current pair of checked recipient
       # if score of the curRecPair is worse (>) than score of checked contender
       if RecipientPrefs[checkedRec][curRecPair] > RecipientPrefs[checkedRec][c]:
-        echo "💔 " & MNames[c] & " prefers " &
-          FNames[checkedRec] & " over " & FNames[contPairs[c]]
-        echo "💔 " & FNames[checkedRec] & " prefers " &
-          MNames[c] & " over " & MNames[curRecPair]
+        echo("💔 ", MNames[c], " prefers ", FNames[checkedRec], " over ", FNames[contPairs[c]])
+        echo("💔 ", FNames[checkedRec], " prefers ", MNames[c], " over ", MNames[curRecPair])
         echo "✗ Unstable"
         return false # unstable
   echo "✓ Stable"
